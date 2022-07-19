f89d7512707fb96c0924ebc092d22d34227c24ac
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2022-07-19 09:46:05 +0200

    Update style and make nav collapsable

--- docs/scripts/nav.js
+++ docs/scripts/nav.js
@@ -15,3 +15,23 @@ function addNavClick(el, to) {
         });
     });
 }
+
+var visible = true;
+
+function toggleNav() {
+    if(visible) {
+        document.getElementById("nav").style.display = "none";
+        document.getElementById("navBtn").style.left = "0px";
+        document.getElementById("navBtn").innerText = ">";
+        document.getElementById("content").style.marginLeft = "32px";
+        document.getElementById("content").style.width = "width: calc(100vw - 4vw)";
+        visible = false;
+    }else {
+        document.getElementById("nav").style.display = "";
+        document.getElementById("navBtn").style.left = "max(200px, 15vw)";
+        document.getElementById("navBtn").innerText = "<";
+        document.getElementById("content").style.marginLeft = "16vw";
+        document.getElementById("content").style.width = "width: calc(100vw - max(200px, 16vw) - 4vw)";
+        visible = true;
+    }
+}
