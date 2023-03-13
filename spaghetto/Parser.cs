92d0f18bc1a25069d1534f447ab18888f8709874
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-03-13 08:08:13 +0100

    Parser work & start eval work

--- spaghetto/Parser.cs
+++ spaghetto/Parser.cs
@@ -25,6 +25,16 @@ namespace spaghetto {
             throw new Exception("Unexpected token " + Current.Type + "; expected " + type);
         }
 
+        public SyntaxToken MatchToken(SyntaxType type, object value)
+        {
+            if (Current.Type == type && Current.Value == value) {
+                Position++;
+                return Peek(-1);
+            }
+
+            throw new Exception("Unexpected token " + Current.Type + "; expected " + type + " with value " + value);
+        }
+
         public Parser(List<SyntaxToken> tokens) {
             Tokens = tokens;
         }
@@ -194,11 +204,14 @@ namespace spaghetto {
             } else if (Current.Type is SyntaxType.Keyword && Current.Value == "if") {
                 return ParseIfExpression();
             } else if (Current.Type is SyntaxType.Keyword && Current.Value == "for") {
-                return ParseForExpression();
+                throw new NotImplementedException();
+                //return ParseForExpression();
             } else if (Current.Type is SyntaxType.Keyword && Current.Value == "while") {
-                return ParseWhileExpression();
+                throw new NotImplementedException();
+                //return ParseWhileExpression();
             } else if (Current.Type is SyntaxType.Keyword && Current.Value == "func") {
-                return ParseFuncExpression();
+                throw new NotImplementedException();
+                //return ParseFuncExpression();
             }
         }
 
@@ -220,7 +233,35 @@ namespace spaghetto {
         }
 
         public SyntaxNode ParseIfExpression() {
+            MatchToken(SyntaxType.Keyword, "if");
 
+            IfNode node = new();
+
+            MatchToken(SyntaxType.LParen);
+            var initialCond = ParseExpression();
+            MatchToken(SyntaxType.RParen);
+
+            var initialBlock = ParseScopedStatements();
+
+            node.AddCase(initialCond, initialBlock);
+
+            do {
+                Position++;
+
+                MatchToken(SyntaxType.LParen);
+                var cond = ParseExpression();
+                MatchToken(SyntaxType.RParen);
+                var block = ParseScopedStatements();
+
+                node.AddCase(cond, block);
+            } while (Current.Type == SyntaxType.Keyword && (string)Current.Value == "elseif");
+
+            if(Current.Type == SyntaxType.Keyword && Current.Value == "else") {
+                Position++;
+                var block = ParseScopedStatements();
+
+                node.AddCase(new LiteralNode(true), block);
+            }
         }
 
         public SyntaxNode BinaryOperation(Func<SyntaxNode> leftParse, List<SyntaxType> allowedTypes, Func<SyntaxNode> rightParse) {
@@ -238,5 +279,146 @@ namespace spaghetto {
 
             return left;
         }
+    }
+
+    internal class BinaryExpressionNode : SyntaxNode
+    {
+        private SyntaxNode left;
+        private SyntaxToken operatorToken;
+        private SyntaxNode right;
+
+        public BinaryExpressionNode(SyntaxNode left, SyntaxToken operatorToken, SyntaxNode right)
+        {
+            this.left = left;
+            this.operatorToken = operatorToken;
+            this.right = right;
+        }
+
+        public override NodeType NodeType => NodeType.BinaryExpression;
+
+        public override SValue Evaluate(Scope scope)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<SyntaxNode> GetChildren()
+        {
+            yield return left;
+            yield return new TokenNode(operatorToken);
+            yield return right;
+        }
+    }
+
+    public abstract class SValue
+    {
+        private static List<PrimitiveCompatibility> primitiveCompatibilities = new()
+        {
+            new(SPrimitiveType.Int, SyntaxType.Plus, SPrimitiveType.Int),
+            new(SPrimitiveType.Int, SyntaxType.Minus, SPrimitiveType.Int),
+            new(SPrimitiveType.Int, SyntaxType.Mul, SPrimitiveType.Int),
+            new(SPrimitiveType.Int, SyntaxType.Div, SPrimitiveType.Int),
+            new(SPrimitiveType.Int, SyntaxType.Mod, SPrimitiveType.Int),
+
+            new(SPrimitiveType.Float, SyntaxType.Plus, SPrimitiveType.Float),
+            new(SPrimitiveType.Float, SyntaxType.Minus, SPrimitiveType.Float),
+            new(SPrimitiveType.Float, SyntaxType.Mul, SPrimitiveType.Float),
+            new(SPrimitiveType.Float, SyntaxType.Div, SPrimitiveType.Float),
+            new(SPrimitiveType.Float, SyntaxType.Mod, SPrimitiveType.Float),
+
+            new(SPrimitiveType.String, SyntaxType.Plus, SPrimitiveType.String),
+            new(SPrimitiveType.String, SyntaxType.Idx, SPrimitiveType.Int),
+        };
+
+        public static SValue Null => new SNumber(0);
+
+        public abstract bool IsPrimitive { get; }
+        public abstract SPrimitiveType PrimitiveType { get; }
+
+        public bool DoPrimitiveActions = false;
+
+        public virtual SValue Add(SValue other)
+        {
+            CheckCompatibility(SyntaxType.Plus, other);
+            return SValue.Null;
+        }
+
+        public virtual SValue Sub(SValue other)
+        {
+            CheckCompatibility(SyntaxType.Minus, other);
+            return SValue.Null;
+        }
+
+        public virtual SValue Mul(SValue other)
+        {
+            CheckCompatibility(SyntaxType.Mul, other);
+            return SValue.Null;
+        }
+
+        public virtual SValue Div(SValue other)
+        {
+            CheckCompatibility(SyntaxType.Div, other);
+            return SValue.Null;
+        }
+
+        public virtual SValue Mod(SValue other)
+        {
+            CheckCompatibility(SyntaxType.Mod, other);
+            return SValue.Null;
+        }
+
+        public virtual SValue Idx(SValue other)
+        {
+            CheckCompatibility(SyntaxType.Idx, other);
+            return SValue.Null;
+        }
+
+
+
+        protected void CheckCompatibility(SyntaxType type, SValue other)
+        {
+            if (this.IsPrimitive && other.IsPrimitive) {
+                DoPrimitiveActions = true;
+
+                if (!primitiveCompatibilities.Contains(new(PrimitiveType, SyntaxType.Plus, other.PrimitiveType))) {
+                    throw new Exception($"Unsupported binary operator + on primitives of type {PrimitiveType} and {other.PrimitiveType}");
+                }
+            }else {
+                // FIXME: Fix when non-primitives are added
+            }
+        }
+    }
+
+    public abstract class SyntaxNode
+    {
+        public abstract NodeType NodeType { get; }
+
+        public abstract object Evaluate(Scope scope);
+        public abstract IEnumerable<SyntaxNode> GetChildren();
+    }
+
+    public struct PrimitiveCompatibility
+    {
+        public SPrimitiveType LHS, RHS;
+        public SyntaxType Operator;
+
+        public PrimitiveCompatibility(SPrimitiveType lhs, SyntaxType op, SPrimitiveType rhs)
+        {
+            LHS = lhs;
+            Operator = op;
+            RHS = rhs;
+        }
+    }
+
+    public enum NodeType
+    {
+        Return,
+        BinaryExpression
+    }
+
+    public enum SPrimitiveType
+    {
+        String,
+        Int,
+        Float,
     }
 }
---
+++ spaghetto/Scope.cs
@@ -0,0 +1,27 @@
+﻿namespace spaghetto
+{
+    public class Scope
+    {
+        public Dictionary<string, object> Table { get; set; } = new();
+        public Scope ParentScope { get; set; }
+
+        public Scope() { }
+        public Scope(Scope parentScope)
+        {
+            ParentScope = parentScope;
+        }
+
+        public object Get(string key)
+        {
+            if (Table.ContainsKey(key)) return Table[key];
+            
+            if(ParentScope == null) return null;
+            return ParentScope.Get(key);
+        }
+
+        public void Set(string key, object value)
+        {
+            Table[key] = value;
+        }
+    }
+}
