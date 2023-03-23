01f25e54d00138aafdf27327299651efdfd67e7d
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-03-23 10:09:49 +0100

    Revert "Clean up"
    This reverts commit 2fe7655905676caf7f8e48915fcb78e4c6b7ad68.

---
+++ SpaghettoIDE/Utils/IO/FileHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaghettoIDE.Utils.IO {
+    internal static class FileHelpers {
+        public static string ReadAllTextOrDefault(string path, string def) {
+            if (!File.Exists(path)) File.WriteAllText(path, def);
+            return File.ReadAllText(path);
+        }
+    }
+}
