6a99107103e363f1a52186c934a954b3b62378c5
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-03-28 09:11:36 +0200

    Update Lib.cs

--- spaghetto.Stdlib.Reflection/Lib.cs
+++ spaghetto.Stdlib.Reflection/Lib.cs
@@ -1,11 +1,14 @@
-﻿namespace spaghetto.Stdlib.Interop {
+﻿/// <summary>
+/// Reflection library provides functions to dynamically interact with SClasses and possibly other types.
+/// The name might be wrong and could be changed in the future.
+/// </summary>
+namespace spaghetto.Stdlib.Reflection {
     public class Lib {
         public static void Mount(Scope scope) {
-            scope.Set("nlimporter$$interop", new SNativeLibraryImporter(Import));
+            scope.Set("nlimporter$$reflection", new SNativeLibraryImporter(Import));
         }
 
         public static void Import(Scope scope) {
-            scope.Set("NETInterop", NETInterop.CreateClass());
         }
     }
 }
