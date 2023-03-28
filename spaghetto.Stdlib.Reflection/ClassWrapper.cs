7dacfef00f2a3241f443140b8fc0f75a5c753559
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-03-28 07:37:25 +0200

    Start adding "Reflection", no real functionality yet

---
+++ spaghetto.Stdlib.Reflection/ClassWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spaghetto.Stdlib.Reflection {
+    public class ClassWrapper {
+        public static SClass CreateClass() {
+            var @class = new SClass("ClassWrapper");
+
+            @class.InstanceBaseTable.Add((new SString("$$ctor"), new SNativeFunction(
+                impl: (Scope scope, List<SValue> args) =>
+                {
+                    if (args[0] is not SClassInstance self) throw new Exception("unexpected error!");
+                    if (args[1] is not SClass @class) throw new Exception("Expected argument 0 to be a class");
+
+                    self.NativeProperties.Add("class", @class);
+                    return self;
+                },
+                expectedArgs: new() { "self", "class" }
+            )));
+
+            // TODO: Add functionality
+            /*@class.InstanceBaseTable.Add((new SString("getSize"), new SNativeFunction(
+                impl: (Scope scope, List<SValue> args) =>
+                {
+                    if (args[0] is not SClassInstance self) throw new Exception("unexpected error!");
+                    if (self.NativeProperties["fileInfo"] is not FileInfo fi) throw new Exception("unexpected error!");
+                    return new SInt((int)(fi).Length);
+                },
+                expectedArgs: new() { "self" },
+                isClassInstanceFunc: true
+            )));*/
+
+            return @class;
+        }
+    }
+}
---
+++ spaghetto.Stdlib.Reflection/Lib.cs
@@ -0,0 +1,11 @@
+﻿namespace spaghetto.Stdlib.Interop {
+    public class Lib {
+        public static void Mount(Scope scope) {
+            scope.Set("nlimporter$$interop", new SNativeLibraryImporter(Import));
+        }
+
+        public static void Import(Scope scope) {
+            scope.Set("NETInterop", NETInterop.CreateClass());
+        }
+    }
+}
