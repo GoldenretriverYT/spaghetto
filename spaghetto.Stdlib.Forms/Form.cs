5448974048682c78c816190927c16a90688b0bf0
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2023-04-13 14:06:59 +0200

    idk

--- spaghetto.Stdlib.Forms/Form.cs
+++ spaghetto.Stdlib.Forms/Form.cs
@@ -1,12 +1,35 @@
 ﻿using System.Reflection;
-
+using WinForm = System.Windows.Forms.Form;
 namespace spaghetto.Stdlib.Interop
 {
     public class Form
     {
+        public static void Main(string[] args) { }
         public static SClass CreateClass()
         {
             var @class = new SClass("Form");
+
+            @class.InstanceBaseTable.Add(("$$ctor", new SNativeFunction(
+                impl: (Scope scope, List<SValue> args) =>
+                {
+                    if (args[0] is not SClassInstance self) throw new Exception("unexpected error!");
+
+                    self.NativeProperties["form"] = new WinForm();
+                    return self;
+                },
+                expectedArgs: new() { "self" }
+            )));
+
+            @class.InstanceBaseTable.Add(("show", new SNativeFunction(
+                impl: (Scope scope, List<SValue> args) =>
+                {
+                    if (args[0] is not SClassInstance self) throw new Exception("unexpected error!");
+
+                    (self.NativeProperties["form"] as WinForm).Show();
+                    return SValue.Null;
+                },
+                expectedArgs: new() { "self" }
+            )));
 
             return @class;
         }
